0d92b4cc0eed4471349dd87b9c6e0cbc049162f6
Johannes May <johannes@may-se.de>
2023-07-18 22:35:47 +0200

    Add a simple flake with the dependencies to run the python inference

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: let pkgs = nixpkgs.legacyPackages.x86_64-linux; in {
+
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [
+        (python3.withPackages (ps: with ps; [ torch configargparse pyyaml torchvision opencv4
+            (
+              buildPythonPackage rec {
+                pname = "piqa";
+                version = "1.3.1";
+                src = fetchPypi {
+                  inherit pname version;
+                  sha256 = "sha256-BwYAlQ1XlgsqgHA9b59z5iH6MVpQqijRPtph4aYubF0=";
+                };
+                doCheck = false;
+                propagatedBuildInputs = [
+                  # Specify dependencies
+                ];
+              }
+            )
+        ]))
+        exiftool
+      ];
+    };
+  };
+}
